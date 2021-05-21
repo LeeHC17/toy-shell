6bc455b8e70e9ceb9246aa386bbb58b522528c12
LeeHC17 <lhchang9@naver.com>
2021-05-21 22:13:57 +0900

    update assignment

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # toy-shell
+- This code should be compiled in 'Linux'(Ubuntu 20.04LTS)
+
+- Add username & hostname. Both username and hostname are represented in MyShell.   If you type 'hostname' or 'username' on MyShell, print out hostname or username.
+
+- Add cwd. Current working directory represented in MyShell.
+
+- Change MyShell color, username and hostname color and cwd color.   MyShell color is light yellow(93). Username and hostname color is light green(92). Cwd color is yellow(33).
+
+- Add exit command. If you type 'exit' on MyShell, print out "exit!!!" and MyShell terminates.
+
+- Support command-line argument. If you type "/bin/ls -l" or "/bin/ls -a" in MyShell, MyShell will show the -l option and -a option.
+
+- If you type "ls", it means "/bin/ls". So, "ls" acts like "/bin/ls". If you type "ls -l" or "ls -a" on MyShell, MyShell will show -l option or -a option like "/bin/ls -l" or "/bin/ls -a".
--- shell.c
+++ shell.c
@@ -1,37 +1,83 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h>                  // 20171331 이현창
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pwd.h>
 
 #define MAX_LEN_LINE    100
-
+#define LEN_HOSTNAME    30
 int main(void)
 {
     char command[MAX_LEN_LINE];
-    char *args[] = {command, NULL};
+    char *args[] = {command,NULL,NULL};
+    char cwd[MAX_LEN_LINE];
     int ret, status;
     pid_t pid, cpid;
     
+    char hostname[LEN_HOSTNAME + 1];
+    memset(hostname, 0x00, sizeof(hostname));    
+    gethostname(hostname, LEN_HOSTNAME);
+
+    getcwd(cwd,sizeof(cwd));                   // get cwd (current working directory)
+
+    
     while (true) {
         char *s;
+	char *option;
         int len;
-        
-        printf("MyShell $ ");
-        s = fgets(command, MAX_LEN_LINE, stdin);
-        if (s == NULL) {
+
+	printf("%c[93mMyShell@", 27);     // color change light yellow
+        printf("%c[92m%s@%s@", 27, getpwuid(getuid())->pw_name,hostname);  /* color change light green
+									     add username & hostname */
+	printf("%c[33m%s $ ", 27,cwd);    //color change yellow, add cwd
+	printf("%c[0m", 27);              // color reset
+	
+	s = fgets(command, MAX_LEN_LINE, stdin);
+
+	if (s == NULL) {
             fprintf(stderr, "fgets failed\n");
             exit(1);
         }
         
         len = strlen(command);
-        printf("%d\n", len);
+        
+	s = strtok(command," ");         // get command
+	option = strtok(NULL,"\n");      // get supporting command-line arguments
+
         if (command[len - 1] == '\n') {
             command[len - 1] = '\0'; 
         }
-        
+	
+	args[0] = command;             // initialize args[0] to input command
+
+	if (!strcmp(command,"ls")){    // 'ls' is same the '/bin/ls'
+		args[0] = "/bin/ls";
+	}
+	
+	if(!strcmp(command,"hostname")){          // print hostname
+		 args[0] = "/bin/hostname";
+	}
+	
+	if(!strcmp(command,"username")){          // print username
+		printf("username: %s\n", getpwuid(getuid())->pw_name);
+		continue;
+	}
+	
+	if(option != NULL){              // input option(supporting command-line arguments) in args[1]
+		args[1] = option;
+	}
+	else{                            
+		args[1] = '\0';          // initialize args[1] to NULL
+	}
+	
+	if(!strcmp(command,"exit")){      // update: add exit command
+		printf("exit!!!\n");
+		return false;
+	}
+	printf("%d\n", len);
         printf("[%s]\n", command);
       
         pid = fork();
@@ -55,7 +101,7 @@ int main(void)
                 fprintf(stderr, "execve failed\n");   
                 return 1;
             }
-        } 
+        }
     }
     return 0;
 }
